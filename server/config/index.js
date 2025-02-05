28b3d532817bd912b12d4a130bd79504659c6ae8
diakel <jinxkey@gmail.com>
2025-02-04 16:40:12 -0800

    changed the env variable name

--- server/config/index.js
+++ server/config/index.js
@@ -8,7 +8,7 @@ const config = {
   PORT: parseInt(process.env.PORT, 10) || 3010,
   AWS: {
     AccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    AWSSecretKey: process.env.AWS_SECRET_KEY,
+    AWSSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
     BucketName: process.env.AWS_S3_BUCKET_NAME,
     Region: "ca-central-1",
   }
