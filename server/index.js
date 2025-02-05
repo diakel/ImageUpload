1a76a98b9832505dfe80bab80d1e3d6257c68da9
diakel <jinxkey@gmail.com>
2025-02-04 16:53:59 -0800

    modified cors permissions

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ app.use(express.json())
 app.use(cors({
     origin: 'http://localhost:3000',
     origin: 'http://localhost:5173',
-    origin: 'http://10.159.23.113:5173'
+    origin: 'http://10.159.23.113:5173',
+    origin: 'https://image-upload-frontend-one.vercel.app/'
 }))
 
 app.use('/api/s3', s3Router)
