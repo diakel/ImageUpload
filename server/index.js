87ed95693c5b5076d60741da9f3ff1f274bd2cfe
diakel <jinxkey@gmail.com>
2025-02-08 14:36:25 -0800

    change for deployment

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: 'http://localhost:3000',
-    origin: 'http://10.43.226.147:5173'
-    //origin: 'https://image-upload-frontend-one.vercel.app',
+    //origin: 'http://10.43.226.147:5173'
+    origin: 'https://image-upload-frontend-one.vercel.app',
     //origin: 'http://192.168.0.102:5173'
 }))
 
