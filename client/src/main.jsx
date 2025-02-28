01326fbcb1a3fa0ee0b9e6b5a0432e4c51c41310
diakel <jinxkey@gmail.com>
2025-02-28 03:11:02 -0800

    ws server name change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-var ws = null;
-/*
-
-const ws = new WebSocket('ws://3.96.63.210:8080')
+const ws = new WebSocket('ws://ec2-3-96-63-210.ca-central-1.compute.amazonaws.com:8080')
 
 ws.onopen = () => {
   console.log('connection to ws established')
 }
-*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
