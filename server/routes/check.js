40c81e0690b0a9b399ea3f6f70031c647f61e7a5
diakel <jinxkey@gmail.com>
2025-03-02 04:41:53 -0800

    load_model without await

--- server/routes/check.js
+++ server/routes/check.js
@@ -10,43 +10,45 @@ const SAFETY_THRESHOLD = 0.5    // if probability that an image is inappropriate
 const nsfwChecker = express.Router()
 const upload = multer()
 
-const _model = await nsfwjs.load();
- 
-const convert = async (imageBuffer) => {
-  // Decoded image in UInt8 Byte array
-  const data = await sharp(imageBuffer).toFormat("jpeg").toBuffer();
-  const image = await jpeg.decode(data, true)
+let _model;
 
-  const numChannels = 3
-  const numPixels = image.width * image.height
-  const values = new Int32Array(numPixels * numChannels)
-  
-  for (let i = 0; i < numPixels; i++)
-    for (let c = 0; c < numChannels; ++c)
-      values[i * numChannels + c] = image.data[i * 4 + c]
-  
-  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
-}
- 
-nsfwChecker.post('/nsfw', upload.single('image'), async (req, res) => {
-  if (!req.file) res.status(400).send('Missing image multipart/form-data')
-  else {
-    const image = await convert(req.file.buffer)
-    const predictions = await _model.classify(image)
-    for (const item of predictions) {
-      if (item.className !== "Drawing" && item.className !== "Neutral") {
-        if (item.probability > SAFETY_THRESHOLD) {
-          return res.send({ answer: "disallow", category: item.className });
-        }
-      }
-    }
-    return res.send({ answer: "allow", category: "" });
-  }
-})
- 
 const load_model = async () => {
   _model = await nsfwjs.load()
 }
+
+load_model().then(() => {
+  const convert = async (imageBuffer) => {
+    // Decoded image in UInt8 Byte array
+    const data = await sharp(imageBuffer).toFormat("jpeg").toBuffer();
+    const image = await jpeg.decode(data, true)
+  
+    const numChannels = 3
+    const numPixels = image.width * image.height
+    const values = new Int32Array(numPixels * numChannels)
+    
+    for (let i = 0; i < numPixels; i++)
+      for (let c = 0; c < numChannels; ++c)
+        values[i * numChannels + c] = image.data[i * 4 + c]
+    
+    return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
+  }
+   
+  nsfwChecker.post('/nsfw', upload.single('image'), async (req, res) => {
+    if (!req.file) res.status(400).send('Missing image multipart/form-data')
+    else {
+      const image = await convert(req.file.buffer)
+      const predictions = await _model.classify(image)
+      for (const item of predictions) {
+        if (item.className !== "Drawing" && item.className !== "Neutral") {
+          if (item.probability > SAFETY_THRESHOLD) {
+            return res.send({ answer: "disallow", category: item.className });
+          }
+        }
+      }
+      return res.send({ answer: "allow", category: "" });
+    }
+  })
+})
 
 //await load_model()
 
