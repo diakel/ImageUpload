dbbdc36d60b9e58b658a582317dbf3109d53a58f
diakel <jinxkey@gmail.com>
2025-03-02 05:37:26 -0800

    load model AGAIN

--- server/routes/check.js
+++ server/routes/check.js
@@ -16,37 +16,39 @@ const load_model = async () => {
 }
 
 load_model().then(() => {
-  const convert = async (imageBuffer) => {
-    // Decoded image in UInt8 Byte array
-    const data = await sharp(imageBuffer).toFormat("jpeg").toBuffer();
-    const image = await jpeg.decode(data, true)
+  console.log("model loaded");
+})
+
+const convert = async (imageBuffer) => {
+  // Decoded image in UInt8 Byte array
+  const data = await sharp(imageBuffer).toFormat("jpeg").toBuffer();
+  const image = await jpeg.decode(data, true)
+
+  const numChannels = 3
+  const numPixels = image.width * image.height
+  const values = new Int32Array(numPixels * numChannels)
   
-    const numChannels = 3
-    const numPixels = image.width * image.height
-    const values = new Int32Array(numPixels * numChannels)
-    
-    for (let i = 0; i < numPixels; i++)
-      for (let c = 0; c < numChannels; ++c)
-        values[i * numChannels + c] = image.data[i * 4 + c]
-    
-    return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
-  }
-   
-  nsfwChecker.post('/nsfw', upload.single('image'), async (req, res) => {
-    if (!req.file) res.status(400).send('Missing image multipart/form-data')
-    else {
-      const image = await convert(req.file.buffer)
-      const predictions = await _model.classify(image)
-      for (const item of predictions) {
-        if (item.className !== "Drawing" && item.className !== "Neutral") {
-          if (item.probability > SAFETY_THRESHOLD) {
-            return res.send({ answer: "disallow", category: item.className });
-          }
+  for (let i = 0; i < numPixels; i++)
+    for (let c = 0; c < numChannels; ++c)
+      values[i * numChannels + c] = image.data[i * 4 + c]
+  
+  return tf.tensor3d(values, [image.height, image.width, numChannels], 'int32')
+}
+  
+nsfwChecker.post('/nsfw', upload.single('image'), async (req, res) => {
+  if (!req.file) res.status(400).send('Missing image multipart/form-data')
+  else {
+    const image = await convert(req.file.buffer)
+    const predictions = await _model.classify(image)
+    for (const item of predictions) {
+      if (item.className !== "Drawing" && item.className !== "Neutral") {
+        if (item.probability > SAFETY_THRESHOLD) {
+          return res.send({ answer: "disallow", category: item.className });
         }
       }
-      return res.send({ answer: "allow", category: "" });
     }
-  })
+    return res.send({ answer: "allow", category: "" });
+  }
 })
 
 //await load_model()
